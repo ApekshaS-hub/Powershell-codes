6e6af037145512fc95b4c2fc18f32e1781410557
ApekshaS-hub <apekshashinde1010@gmail.com>
2024-07-31 12:58:56 +0530

    Update GetInfo_DeepCleanUp.ps1

--- GetInfo_DeepCleanUp.ps1
+++ GetInfo_DeepCleanUp.ps1
@@ -1,26 +1,33 @@
-
-function Get-FolderSize {
+function Get-FolderSizes {
     param (
-        [string]$Path
+        [hashtable]$Paths
     )
-    
-    if (-Not (Test-Path -Path $Path)) {
-        Write-Warning "Path $Path does not exist."
-        return 0
+    $totalSize = 0
+
+    foreach ($name in $Paths.Keys) {
+        if (-Not (Test-Path -Path $Paths[$name])) {
+            Write-Warning "Path $($Paths[$name]) does not exist."
+            continue
+        }
+
+        try {
+            $files = Get-ChildItem -Path $Paths[$name] -Recurse -File -ErrorAction Stop
+            $size = ($files | Measure-Object -Property Length -Sum).Sum / 1MB
+            $size = [math]::Round($size, 2)
+        }
+        catch {
+            Write-Warning "Failed to get size for path $($Paths[$name]). Error: $_"
+            $size = 0
+        }
+
+        Write-Output "Total size of {$name}: $size MB"
+        $totalSize += $size
     }
-    
-    try {
-        $files = Get-ChildItem -Path $Path -Recurse -File -ErrorAction Stop
-        $totalSize = ($files | Measure-Object -Property Length -Sum).Sum / 1MB
-        return [math]::Round($totalSize, 2)
-    }
-    catch {
-        Write-Warning "Failed to get size for path $Path. Error: $_"
-        return 0
-    }
+
+    Write-Output "Total size of all specified directories: $totalSize MB"
+    Write-Output "********************************************************"
 }
 
-# Paths to the directories
 $paths = @{
     "Windows Update Cleanup" = "C:\Windows\SoftwareDistribution\DataStore\Logs"
     "Windows Error Reports and Feedback Diagnostics" = "C:\ProgramData\Microsoft\Windows\WER\ReportArchive"
@@ -28,16 +35,4 @@ $paths = @{
     "Temporary Files" = [System.IO.Path]::GetTempPath()
 }
 
-$totalSize = 0
-
-# Calculate sizes and display output
-foreach ($name in $paths.Keys) {
-    $size = Get-FolderSize -Path $paths[$name]
-    Write-Output "Total size of {$name}: $size MB"
-    $totalSize += $size
-}
-
-# Display total size of all directories
-Write-Output "Total size of all specified directories: $totalSize MB"
-
-Write-Output "********************************************************"
+Get-FolderSizes -Paths $paths
